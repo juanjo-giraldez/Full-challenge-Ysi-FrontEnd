5934ccee1c8fcc9ebb92b04c87d3fe460cd28510
juanjo giraldez <juanjose.giraldez@kairosds.com>
2020-10-04 17:20:09 +0200

    in models/companies we create the data arrival format for request to REST API and the server

--- app/models/companies.js
+++ app/models/companies.js
@@ -1,5 +1,9 @@
-import Model from '@ember-data/model';
+import DS from 'ember-data';
 
-export default class CompaniesModel extends Model {
-
-}
+export default DS.Model.extend({
+    Name: DS.attr('string'),
+    Direction: DS.attr('string'),
+    Phone: DS.attr('string'),
+    Web: DS.attr('string'),
+    CreatedAt: DS.attr('string'),
+});
